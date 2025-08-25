8311423b3904aa6e299d53e134f7db140c2b2684
gunjan2005-tech <gunjanu0902@gmail.com>
2025-08-25 21:07:33 +0530

    Fix import path for CartSlice

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { CartSlice } from "./Slices/cartSlice";
-import cartReducer from "./Slices/cartSlice"; 
+
+import cartReducer from "./Slices/cartSlice";
+
 const Store = configureStore({
     reducer : {
         cart : cartReducer,
