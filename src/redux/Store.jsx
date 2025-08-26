f3a00e4850a3a4af9a1abbfdb79d656eed8efd3e
gunjan2005-tech <gunjanu0902@gmail.com>
2025-08-26 11:06:27 +0530

    Rename redux folder to slices and update imports

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "./slices/cartSlice";
 
+
+
 const Store = configureStore({
     reducer : {
         cart : cartReducer,
